f02bc6b73658ca34414c090f24c2d889027b0a5f
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-06-11 11:53:30 +0530

    Update Day-10(Search Insert Position).cpp

--- Day-10(Search Insert Position).cpp
+++ Day-10(Search Insert Position).cpp
@@ -25,3 +25,33 @@ public:
      return index;   
     }
 };
+
+/*
+Time complexity : O(logn)
+Space complexity : O(1)
+Approach 2 : Use of Binary Search to find the target index
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int start=0;
+        int end=nums.size()-1;
+        if(target>nums[nums.size()-1])
+            return nums.size();
+        int mid= start+(end-start)/2;
+        while(end>start)
+        {
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]> target)
+                end=mid;
+            else
+                start=mid+1;
+           mid=start+(end-start)/2; 
+        }
+        
+        return start;
+    }
+};
