0d9ff63c22a180420b66c9da656588a919a17438
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-06-29 13:38:32 +0530

    Create Day-29(Unique Paths).cpp

---
+++ Day-29(Unique Paths).cpp
@@ -0,0 +1,30 @@
+/* 
+Time complexity : O(n^2)
+Space complexity : O(n*m)
+My approach : Use of DP to calculate the unique paths.
+*/
+class Solution 
+{
+public:
+    int uniquePaths(int m, int n) 
+    {
+        int dp[m][n];
+        memset(dp,0,sizeof(dp));
+        for(int j=0;j<n;j++)
+        {
+            dp[0][j]=1;
+        }
+        for(int i=0;i<m;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+      return dp[m-1][n-1];  
+    }
+};
